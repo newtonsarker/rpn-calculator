a674f2b21ce5e7099451359786de27f826dbe132
newton <newton.sarker@fortumo.com>
2021-11-30 02:58:10 +0200

    Implemented servlet

---
+++ src/main/java/com/newton/calculator/rpn/exceptions/InvalidHttpRequestException.java
@@ -0,0 +1,8 @@
+package com.newton.calculator.rpn.exceptions;
+
+public class InvalidHttpRequestException extends Exception {
+
+    public InvalidHttpRequestException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/newton/calculator/rpn/servlets/CalculatorServlet.java
+++ src/main/java/com/newton/calculator/rpn/servlets/CalculatorServlet.java
@@ -1,17 +1,37 @@
 package com.newton.calculator.rpn.servlets;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.newton.calculator.rpn.exceptions.InvalidHttpRequestException;
+import com.newton.calculator.rpn.exceptions.MalformedExpressionException;
+import com.newton.calculator.rpn.service.RpnService;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 public class CalculatorServlet extends HttpServlet {
 
+    private final RpnService rpnService = new RpnService();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
-        resp.setStatus(HttpServletResponse.SC_OK);
-        resp.getWriter().println("greeting");
+        try {
+            final var expression = this.extractExpressionFromPath(req);
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println(this.rpnService.calculate(expression));
+        } catch (InvalidHttpRequestException | MalformedExpressionException exception) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().println(exception.getMessage());
+        }
+    }
 
+    private String extractExpressionFromPath(HttpServletRequest req) throws InvalidHttpRequestException {
+        final var pathVariables = req.getPathInfo().split("/");
+        if (pathVariables[1] != null && pathVariables[1].length() > 0) {
+            return pathVariables[1];
+        }
+        throw new InvalidHttpRequestException("Expression not available");
     }
 }
